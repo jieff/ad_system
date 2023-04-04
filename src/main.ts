33be50c6135d1e3cec6a44916e8d13e444943bec
Jieff Cavalcanti <jieff.cavalcanti@hotmail.com>
2023-04-04 08:55:51 -0300

    implementação lib cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use('/users', cors());
   await app.listen(4000);
 }
 bootstrap();
