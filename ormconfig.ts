3a6865228b83acc0dd05bca74838c00b731fe0a4
Servidor <jieff.cavalcanti.com>
2023-04-06 16:42:18 -0300

    update database

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,11 +3,7 @@ import { DataSourceOptions } from 'typeorm';
 export const config: DataSourceOptions = {
   type: 'mysql',
   host: 'localhost',
-<<<<<<< HEAD
   port: 3307,
-=======
-  port: 3706,
->>>>>>> 8e8d0a02bd3063cb767d6ad043c7629f760edfd9
   username: 'root',
   password: 'root',
   database: 'ad',
