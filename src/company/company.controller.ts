8e8d0a02bd3063cb767d6ad043c7629f760edfd9
Jieff Cavalcanti <jieff.cavalcanti@gmail.com>
2023-04-06 16:28:26 -0300

    implementação swagger

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -2,31 +2,38 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CompanyService } from './company.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('Cadastra Empresas Administradora do Sistema')
 @Controller('company')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
+  @ApiOperation({ summary: 'Rota para cadastrar Empresa ADM' })
   @Post()
   create(@Body() createCompanyDto: CreateCompanyDto) {
     return this.companyService.create(createCompanyDto);
   }
 
+  @ApiOperation({ summary: 'Rota para listar todas as empresas ADM' })
   @Get()
   findAll() {
     return this.companyService.findAll();
   }
 
+  @ApiOperation({ summary: 'Rota para listar uma empresa por id' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.companyService.findOne(+id);
   }
 
+  @ApiOperation({ summary: 'Rota para realizar a atualização dos dados por id' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
     return this.companyService.update(+id, updateCompanyDto);
   }
 
+  @ApiOperation({ summary: 'Rota para apagar uma empresa ADM' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.companyService.remove(+id);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,31 +2,38 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('Formulário para cadastrar empresas')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Rota para cadastro de empresa' })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @ApiOperation({ summary: 'Rota para listar todas empresas' })
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @ApiOperation({ summary: 'Rota para listar uma empresa por id' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
+  @ApiOperation({ summary: 'Rota para atualizar dados de uma empresa por id' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
+  @ApiOperation({ summary: 'Rota apagar uma empresa por id' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
